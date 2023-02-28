9b0ee0db5ea49f118c5405677488864ece9a03ff
Marcelo Leiva <leivaskt@gmail.com>
2023-02-28 00:15:48 -0300

    hice el ejer 6 y 7, el 7 esta bien pero tiene algo raro si ingreso 11 en el primero numero 5 en el segundo y 3 en el ultimo toma el 5 como el maximo

---
+++ js/Ejer6.js
@@ -0,0 +1,9 @@
+let numero1 = prompt('Ingrese un numero');
+let numero2 = prompt('ingrese un segundo numero');
+
+if(numero1 > numero2){
+    document.write('Es Mayor ', numero1);
+}
+else{
+    document.write('Es Mayor ', numero2);
+}
