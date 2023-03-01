cf04ef8a9dde59004d8eec4a8b1576443e354b27
Marcelo Leiva <leivaskt@gmail.com>
2023-03-01 15:24:15 -0300

    termine ejer 11

--- js/Ejer11.js
+++ js/Ejer11.js
@@ -1,10 +1,13 @@
 let numero = prompt('ingrese un numero');
-let div = 2
-let div2 = 3
-let div3 = 5
-let div4 = 7
-if(numero % div===0 && numero % div2 === 0 && numero % div3 ===0 && numero % div4 ===0){
+
+if(numero % 2===0 && numero % 3=== 0 && numero % 5===0 && numero % 7===0){
     document.write('Es divisible por todos');
-}else if(numero % div===0 || numero % 3 === 0 || numero % 5 ===0 || numero % 7 ===0){
-    document.write('es divisible por ',div,div2 );
+}else if(numero % 2===0 ){
+    document.write('Es divisible por 2');
+}else if(numero % 3 ===0){
+        document.write('Es divisible por 3');
+}else if(numero % 5 ===0){
+        document.write('Es divisible por 5');
+}else if (numero % 7 ===0){
+    document.write('Es divisible por 7');
 }
